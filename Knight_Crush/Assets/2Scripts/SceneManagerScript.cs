412999e692defc9fcf372a63aa37f8cb2e7d7aad
ChoiYJ97 <50546899+ChoiYJ97@users.noreply.github.com>
2021-07-05 15:30:39 +0900

    Scene Added
    SceneManager added
    Scene added

---
+++ Knight_Crush/Assets/2Scripts/SceneManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    static SceneManagerScript UniqueInstance;
+    public static SceneManagerScript _instance
+    {
+        get { return UniqueInstance; }
+    }
+
+    public enum CurScene
+    {
+        EmptyS = 0,
+        LobbyS,
+        
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    void GoLobbyScene()
+    {
+        SceneManager.LoadScene("LobbyScene");
+    }
+
+    void GoInGameScene()
+    {
+        SceneManager.LoadScene("InGameScene");
+    }
+
+    void GoStageSelectScene()
+    {
+        
+    }
+
+    void GoResultScene()
+    {
+
+    }
+
+    void GoCharacterMakingScene()
+    {
+
+    }
+
+}
